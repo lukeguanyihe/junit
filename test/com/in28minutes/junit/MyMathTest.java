9349400c8fe96d59c82abf099dbc752788e49b4a
Luke Guan <lukeguan@outlook.com>
2022-03-19 23:53:39 -0700

    unit test

--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -5,9 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyMathTest {
-
+	MyMath myMath = new MyMath();
+	
+	
 	@Test
-	public void test() {
+	public void sum_with3numbers() {
+		assertEquals(6, myMath.sum(new int[] {1,2,3}));
+	}
+	
+	@Test
+	public void sum_with1numbers() {
+		assertEquals(3, myMath.sum(new int[] {3}));
 	}
 
 }
